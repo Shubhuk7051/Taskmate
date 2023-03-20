78bd640220b89b7540516d904089dabe801829ea
Shubham <shubhamkatre7051@gmail.com>
2023-03-20 23:14:50 +0530

    Whitenoise Setting

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -97,6 +98,7 @@ DATABASES = {
         'PORT' : env('DJANGO_DB_PORT'),
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
